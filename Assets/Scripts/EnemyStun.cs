11f89c73839febd3790aac74b9670719f7366144
Hofbrand <hofbrand.yauheni@gmail.com>
2020-11-20 09:13:33 +0300

    Init commit
    INIT

---
+++ Assets/Scripts/EnemyStun.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using JetBrains.Annotations;
+
+public class EnemyStun : MonoBehaviour {
+
+	public bool boss = false;
+	public int bossLives = 3;
+	public int lives = 0;
+
+	// if Player hits the stun point of the enemy, then call Stunned on the enemy
+	void OnCollisionEnter2D(Collision2D other)
+	{
+	
+		if (other.gameObject.tag == "Player")
+		{
+			// tell the enemy to be stunned
+			this.GetComponentInParent<Enemy>().Stunned();
+			other.gameObject.GetComponent<CharacterController2D>().EnemyBounce();
+			if (boss)
+			{
+				if (bossLives > 0)
+				{
+					bossLives -= 1;
+				}
+				else
+				{
+					other.gameObject.GetComponent<CharacterController2D>().Victory();
+					Destroy(this.GetComponentInParent<Enemy>());
+				}
+			}
+            else
+            {
+                if (lives > 0)
+                {
+					lives--;
+                }
+                else
+                {
+					Destroy(transform.parent.gameObject);
+                }
+
+            }
+		}
+	}
+}
---
+++ Assets/Scripts/Heal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heal : MonoBehaviour
+{
+	//hp
+	const int points = 10;
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Player")
+		{
+			CharacterController2D player = collision.gameObject.GetComponent<CharacterController2D>();
+			player.ApplyHeal(points);
+			Destroy(gameObject);
+		}
+	}
+}
